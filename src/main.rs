9b5bd85b5bf25f3db3d41f27048ef3e0ba43ccdf
Lulzmatt27 <lulmatt27@gmail.com>
2020-11-05 21:55:29 +0300

    example simple thread and mpsc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
 fn main() {
-    println!("Hello, world!");
+    thread_example();
+    message_passing();
+}
+
+fn thread_example(){
+    let t1 = thread::spawn(||{
+        for i in 1..10{
+            println!("Greeting {} from other thread!", i);
+            thread::sleep(Duration::from_millis(100));
+        }
+    });
+
+    for i in 1..5{
+        println!("Greeting {} from main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+    t1.join().unwrap();
+
+}
+fn message_passing(){
+    let (tx1, rx) = mpsc::channel();
+    let tx2 = mpsc::Sender::clone(&tx1);
+
+    thread::spawn(move || {
+        println!("Sending message from thread 1");
+        tx1.send(String::from("greeting from thread 1")).unwrap();
+    });
+
+    thread::spawn(move||{
+        println!("Sending message from thread 2");
+        tx2.send(String::from("greeting from thread 2")).unwrap();
+    });
+
+    for recvd in rx{
+        println!("Received: {}", recvd);
+    }
 }

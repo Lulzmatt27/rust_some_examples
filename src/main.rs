9ff73ecc7eee207bbd8afad39e3f5449ac464d3c
Lulzmatt27 <lulmatt27@gmail.com>
2020-11-05 22:13:11 +0300

    add arc+mutex example

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
+use std::sync::{Arc, Mutex};
+use std::cell::RefCell;
 fn main() {
     thread_example();
     message_passing();
+    with_ref_count();
 }
 
 fn thread_example(){
@@ -38,4 +41,21 @@ fn message_passing(){
     for recvd in rx{
         println!("Received: {}", recvd);
     }
+}
+fn with_ref_count(){
+    let counter = Arc::new(Mutex::new(0));
+    let mut threads = vec![];
+
+    for _ in 0..5{
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move ||{
+            let mut num_guard = (*counter).lock().unwrap();
+            *num_guard +=1;
+        });
+        threads.push(handle);
+    }
+    for thrd in threads{
+        thrd.join().unwrap();
+    }
+    println!("Result: {}", *counter.lock().unwrap());
 }
